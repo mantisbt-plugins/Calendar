891daa1d581eb7fcd05e045ecd86c672001c85f9
ermolaev <igflocal@gmail.com>
2018-06-25 15:20:46 +0700

    Release 2.3.3

--- Calendar.php
+++ Calendar.php
@@ -82,7 +82,7 @@ class CalendarPlugin extends MantisPlugin {
         $this->description = plugin_lang_get( 'description' );
         $this->page        = 'config_page';
 
-        $this->version = '2.3.3-dev';
+        $this->version = '2.3.3';
 
         $this->requires = array(
                                   'MantisCore' => '2.14.0',
