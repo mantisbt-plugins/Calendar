a92882ad7b5d93ddeb19b5937da32c6f9c7a846a
ermolaev <igflocal@gmail.com>
2019-03-29 14:13:32 +0700

    Bump version to 2.5.0 for release.

--- Calendar.php
+++ Calendar.php
@@ -138,7 +138,7 @@ class CalendarPlugin extends MantisPlugin {
         $this->description = plugin_lang_get( 'description' );
         $this->page        = 'config_page';
 
-        $this->version = '2.5.0-dev';
+        $this->version = '2.5.0';
 
         $this->requires = array(
                                   'MantisCore' => '2.14.0',
